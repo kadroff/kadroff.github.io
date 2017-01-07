8151bb18d440608c3bf29a150423131a1fadf5ca
Zurab Shivarbidze <zybpwnz@gmail.com>
2017-01-07 23:43:58 +0200

    Zurab's fixes

--- js/app.js
+++ js/app.js
@@ -4,8 +4,8 @@ $(document).ready(function() {
   });
 
   // $(window).on('load', function () {
-  //   var $preloader = $('#page-preloader'),
-  //   $spinner   = $preloader.find('.spinner');
-  //   $spinner.fadeOut();
-  //   $preloader.delay(350).fadeOut('slow');
+    var $preloader = $('#page-preloader'),
+        $spinner = $preloader.find('.spinner');
+    $spinner.fadeOut();
+    $preloader.delay(3500).fadeOut('slow');
 });
