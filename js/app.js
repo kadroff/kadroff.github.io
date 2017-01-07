88d5a3b1b9c61ca1ae18418cbc5208a659ee05cb
Marsel Kadrov <marselkadroff@gmail.com>
2017-01-08 01:18:27 +0500

    version 1.0 update-2

--- js/app.js
+++ js/app.js
@@ -3,10 +3,10 @@ $(document).ready(function() {
     anchors: ['header', 'skills','portfolio','contacts'],
   });
 
-  $(window).on('load', function () {
-    var $preloader = $('#page-preloader'),
-    $spinner   = $preloader.find('.spinner');
-    $spinner.fadeOut();
-    $preloader.delay(350).fadeOut('slow');
+  // $(window).on('load', function () {
+  //   var $preloader = $('#page-preloader'),
+  //   $spinner   = $preloader.find('.spinner');
+  //   $spinner.fadeOut();
+  //   $preloader.delay(350).fadeOut('slow');
 });
 });
