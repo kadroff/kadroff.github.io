5eaa4d65a55f27667b06b9e7f98759f4ce218899
Marsel <marsel.kadroff@gmail.com>
2018-05-15 02:14:51 +0500

    Delete preloader

--- js/app.js
+++ js/app.js
@@ -2,10 +2,4 @@ $(document).ready(function() {
   $('#fullpage').fullpage({
     anchors: ['header', 'skills','portfolio','contacts'],
   });
-
-  // $(window).on('load', function () {
-    var $preloader = $('#page-preloader'),
-        $spinner = $preloader.find('.spinner');
-    $spinner.fadeOut();
-    $preloader.delay(3500).fadeOut('slow');
 });
